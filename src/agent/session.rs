332847681a228e319cfa9e3512baee59381a5232
IASONAS STEFANOU <sdi2200246@di.uoa.gr>
2026-02-25 19:43:09 +0200

    Refactor AgentSession and add ConversationEvent enum

--- src/agent/session.rs
+++ src/agent/session.rs
@@ -1,40 +1,25 @@
+use crate::agent::tools::capability::ToolFunction;
 use serde_json::Value;
 
-use super::protocol::message::Message;
-use super::protocol::tool::{Tool};
+#[derive(Debug , PartialEq)]
+pub enum ConversationEvent {
+    System(String),
+    User(String),
+    ToolCall {
+        name: String,
+        arguments: Value,
+        id: String,
+    },
+    ToolResult {
+        name: String,
+        result: String,
+        id: String,
+    },
+}
 
 #[derive(Debug)]
 pub struct AgentSession{
-    pub model:String,
-    pub messages:Vec<Message>,
-    pub tools:Vec<Tool>,
+    pub events:Vec<ConversationEvent>,
+    pub available_tools:Vec<ToolFunction>,
     pub steps:usize,
-    pub contract:Value,
-}
-
-impl AgentSession {
-    pub fn new(model:&str , messages:Vec<Message> , tools:Vec<Tool> , steps:usize , contract:Value)->AgentSession{
-        AgentSession {model:model.into(), messages, tools, steps, contract}
-    }
-    pub fn tool_result(&mut self, message: Message) -> &mut Self {
-        self.messages.push(message);
-        self
-    }
-    pub fn model_res(&mut self , message:Message) ->&mut Self{
-        self.messages.push(message);
-        self
-    }
-    pub fn error(&mut self, er:String) -> &mut Self{
-        let message = Message::system(Some(er));
-        self.messages.push(message);
-        self
-    } 
-
-    pub fn decrease_steps(&mut self){
-            self.steps -= 1;
-    }
-    pub fn steps(&self)->usize{
-        self.steps
-    }
-
 }
