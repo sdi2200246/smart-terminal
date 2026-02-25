612aab9a7d99d08d3249722b938fa7a62710e3af
IASONAS STEFANOU <sdi2200246@di.uoa.gr>
2026-02-25 19:37:40 +0200

    react: Moved  protocol module files

--- src/protocol/error.rs
+++
@@ -1,7 +0,0 @@
-use serde::Deserialize;
-
-#[derive(Deserialize , Debug)]
-pub enum ProtocolError{
-    Empty,
-    NoTool,
-}
--- src/protocol/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod message;
-pub mod responce;
-pub mod tool;
-pub mod model_result;
-pub mod request;
-pub mod error;
-use super::agent;
