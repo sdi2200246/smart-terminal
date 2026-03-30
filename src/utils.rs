ebf8c4cf21862b7b2ef80a1a69d03776998f53c6
IASONAS STEFANOU <sdi2200246@di.uoa.gr>
2026-03-30 20:14:35 +0300

    chore:remove uneccessary comments

--- src/utils.rs
+++ src/utils.rs
@@ -4,19 +4,11 @@ use schemars::generate::SchemaSettings;
 
 pub trait FlatSchema : JsonSchema{
     fn schema() -> Value {
-        // 1. Create settings that force inlining
         let settings = SchemaSettings::draft07().with(|s| {
             s.inline_subschemas = true;
         });
-        
-        // 2. Create a generator with those settings
         let r#gen = settings.into_generator();
-        
-        // 3. Generate the schema for the current type
         let schema = r#gen.into_root_schema_for::<Self>();
-        
-        // 4. Convert to serde_json::Value
         serde_json::to_value(schema).unwrap()
     }
-
 }
