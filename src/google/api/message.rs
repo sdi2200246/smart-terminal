9e6bb6c522d25ed3c73785c1d7383c90ccdd88e5
Jason Stefanou <sdi2200246@di.uoa.gr>
2026-08-08 15:22:53 +0300

    Add Google Gemini API client modules and structs

---
+++ src/google/api/message.rs
@@ -0,0 +1,103 @@
+use super::tool::{FunctionCall, FunctionResponse};
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct Part {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub text: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub function_call: Option<FunctionCall>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub function_response: Option<FunctionResponse>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+pub struct Message {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub role: Option<String>,
+    pub parts: Vec<Part>,
+}
+
+impl Message {
+    pub fn user(content: Option<String>) -> Message {
+        Message {
+            role: Some("user".into()),
+            parts: vec![Part {
+                text: content,
+                function_call: None,
+                function_response: None,
+            }],
+        }
+    }
+
+    pub fn system(content: Option<String>) -> Message {
+        Message {
+            role: None,
+            parts: vec![Part {
+                text: content,
+                function_call: None,
+                function_response: None,
+            }],
+        }
+    }
+
+    pub fn context<T: Serialize>(ctx: &T) -> Message {
+        let json = serde_json::to_string_pretty(ctx).unwrap();
+        let content = format!("Context:\n{}", json);
+
+        Message::system(Some(content))
+    }
+
+   pub fn tool_responce(content: Option<String>, tool_name: String) -> Message {
+        Message {
+            role: Some("function".into()),
+            parts: vec![Part {
+                text: None,
+                function_call: None,
+                function_response: Some(FunctionResponse {
+                    name: tool_name,
+                    response: content.map(|c| serde_json::json!({ "content": c })),
+                }),
+            }],
+        }
+    }
+
+    pub fn tool_call(name: String, args: Value) -> Message {
+        Message {
+            role: Some("model".into()),
+            parts: vec![Part {
+                text: None,
+                function_call: Some(FunctionCall { name, args }),
+                function_response: None,
+            }],
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde::Serialize;
+
+    #[derive(Serialize)]
+    struct TestContext {
+        cwd: String,
+        history_len: usize,
+    }
+
+    #[test]
+    fn context_creates_expected_message() {
+        let ctx = TestContext {
+            cwd: "/tmp".into(),
+            history_len: 42,
+        };
+
+        let msg = Message::context(&ctx);
+
+        println!("CONTENT:\n{:?}", msg.parts[0].text);
+    }
+}
---
+++ src/google/api/tool.rs
@@ -0,0 +1,36 @@
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
+#[derive(Serialize, Deserialize, PartialEq, Eq, JsonSchema, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct  Tool {
+    #[serde(skip_serializing_if = "Vec::is_empty", default)]
+    pub function_declarations: Vec<FunctionDeclaration>,
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Eq, JsonSchema, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct FunctionDeclaration {
+    pub name: String,
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub description: Option<String>,
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Value::is_null")]
+    pub parameters: Value,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct FunctionCall {
+    pub name: String,
+    pub args: Value,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct FunctionResponse {
+    pub name: String,
+    pub response: Option<Value>,
+}
