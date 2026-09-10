d2ae4c4f29a2dcdf713a6b1699925b745f23122c
Jason Stefanou <sdi2200246@di.uoa.gr>
2026-09-10 11:16:26 +0300

    Remove daily rotation of logs

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,7 @@ async fn main() {
         .unwrap_or_else(|| std::path::PathBuf::from("."))
         .join("logs");
 
-    let file_appender = tracing_appender::rolling::daily(log_dir, "app.log");
+    let file_appender = tracing_appender::rolling::never(log_dir, "app.log");
     let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
     tracing_subscriber::registry()
         .with(
